8f592286eff17ff2800e0df7e358ea8f1d7d71bd
tek <torstenschmits@gmail.com>
2016-12-19 20:53:17 +0100

    more test cases

--- unit/src/basic.scala
+++ unit/src/basic.scala
@@ -5,30 +5,49 @@ package object bp
   class ***[A, B]
   class >:<[A, B]
 
-  trait C
+  class C
   trait D
+  trait E
+  object F
+
+  type CAux[B] = C { type A = B }
 
   type T1 = C *** D >:< (C with D { type A = D; type B = C })
   type T2 = D *** ((C >:< C) *** (D => Unit))
   type T3 = (D *** (C *** String)) >:< ((C, D, C) *** D)
 
-  implicit val c1: C = null
+  implicit val c1: C = ???
 
-  implicit val c2: C = null
+  implicit val c2: C = ???
 
-  implicit def e(implicit impPar2: T3): T2 = null
+  implicit def e(implicit impPar2: T3): T2 = ???
 
-  implicit def d(implicit impPar1: T2): T1 = null
+  implicit def d(implicit impPar1: T2): T1 = ???
+
+  implicit def f1: D = ???
+
+  implicit def f2: D = ???
+
+  implicit def h(implicit impPar4: D): E = ???
 }
 
 object A
 {
-  import bp.{C, D, T1, ***, >:<}
+  import bp.{C, CAux, D, F, T1, ***, >:<}
   val a = new (Int *** ((C *** String) >:< D))
-  def b(arg: Int *** ((String *** C) >:< D)) = null
-  def c(implicit impPar0: T1) = null
+  def b(arg: Int *** ((String *** C { type A = F.type }) >:< D)) = ???
+  def c(implicit impPar0: T1) = ???
+  def i(arg: CAux[D]) = ???
   a.attr
   b(a)
   c
-  implicitly[C]
+  val p1 = new C { type A = C }
+  i(p1)
+}
+
+object B {
+  trait C[T]
+  implicit def c[A, B](implicit tpeS: C[A], tpeT: C[B]): C[A with B] = ???
+  def as[A](implicit tpe: C[A]) = ???
+  as[Any]
 }
