1340fe051185cfbb6c558e295b51fc4063c0eddc
Torsten Schmits <torstenschmits@gmail.com>
2020-04-30 17:31:14 +0200

    remove obsolete implicit statistic

--- src/main/scala/splain/ImplicitStats.scala
+++ src/main/scala/splain/ImplicitStats.scala
@@ -8,14 +8,12 @@ trait ImplicitStats
   import statistics._
 
   def withImplicitStats[A](run: () => A) = {
-    val rawTypeStart = if (StatisticsStatics.areSomeColdStatsEnabled) statistics.startCounter(rawTypeImpl) else null
     val findMemberStart =
       if (StatisticsStatics.areSomeColdStatsEnabled) statistics.startCounter(findMemberImpl) else null
     val subtypeStart = if (StatisticsStatics.areSomeColdStatsEnabled) statistics.startCounter(subtypeImpl) else null
     val start = if (StatisticsStatics.areSomeColdStatsEnabled) statistics.startTimer(implicitNanos) else null
     val result = run()
     if (StatisticsStatics.areSomeColdStatsEnabled) statistics.stopTimer(implicitNanos, start)
-    if (StatisticsStatics.areSomeColdStatsEnabled) statistics.stopCounter(rawTypeImpl, rawTypeStart)
     if (StatisticsStatics.areSomeColdStatsEnabled) statistics.stopCounter(findMemberImpl, findMemberStart)
     if (StatisticsStatics.areSomeColdStatsEnabled) statistics.stopCounter(subtypeImpl, subtypeStart)
     result
