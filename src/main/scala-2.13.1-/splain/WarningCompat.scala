91e4011e80a916245df4853fbb8707a23a17bbd6
Torsten Schmits <torstenschmits@gmail.com>
2021-06-25 11:27:46 +0200

    add missing argument to contextWarning call

--- src/main/scala-2.13.1-/splain/WarningCompat.scala
+++ src/main/scala-2.13.1-/splain/WarningCompat.scala
@@ -1,9 +1,11 @@
 package splain
 
+import tools.nsc.Reporting.WarningCategory
+
 trait WarningCompat
 { self: Analyzer =>
   import global._
 
   def contextWarning(context: Context, pos: Position, message: String): Unit =
-    context.warning(pos, message)
+    context.warning(pos, message, WarningCategory.Other)
 }
