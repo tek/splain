61e742101f0e162e984f5014a5ed08198894e61a
Peng Cheng <pc175@uow.edu.au>
2021-10-18 18:07:40 -0400

    fix 2 disabled unit tests caused by using empty annotationMsg

--- core/src/main/scala-2.13.6+/splain/SplainAnalyzer.scala
+++ core/src/main/scala-2.13.6+/splain/SplainAnalyzer.scala
@@ -4,6 +4,8 @@ import scala.tools.nsc._
 
 class SplainAnalyzer(val global: Global) extends typechecker.Analyzer with SplainFormattingExtension {
 
+  import global._
+
   override val specialFormatters: List[SpecialFormatter] =
     List(
       FunctionFormatter,
@@ -14,4 +16,38 @@ class SplainAnalyzer(val global: Global) extends typechecker.Analyzer with Splai
       ByNameFormatter
     )
 
+//  override def NoImplicitFoundError(tree: Tree, param: Symbol)(
+//      implicit
+//      context: Context
+//  ): Unit = {
+//    val (isSupplement, annotationMsg) = NoImplicitFoundAnnotation(tree, param)
+//    def defaultErrMsg = {
+//      val paramName = param.name
+//      val paramTp = param.tpe
+//      def evOrParam =
+//        if (paramName startsWith nme.EVIDENCE_PARAM_PREFIX)
+//          "evidence parameter of type"
+//        else
+//          s"parameter $paramName:"
+//      if (annotationMsg == null || annotationMsg.isEmpty)
+//        s"could not find implicit value for $evOrParam $paramTp$annotationMsg"
+//      else annotationMsg
+//    }
+//    val errMsg = splainPushOrReportNotFound(tree, param, annotationMsg)
+//    ErrorUtils.issueNormalTypeError(tree, if (errMsg.isEmpty) defaultErrMsg else errMsg)
+//  }
+//
+//  override def splainPushOrReportNotFound(tree: Tree, param: Symbol, annotationMsg: String): String = {
+//    if (settings.Vimplicits)
+//      if (ImplicitErrors.nested) {
+//        splainPushNotFound(tree, param)
+//        ""
+//      } else
+//        pluginsNoImplicitFoundError(
+//          param,
+//          ImplicitErrors.errors,
+//          formatImplicitError(param, ImplicitErrors.errors, annotationMsg)
+//        )
+//    else ""
+//  }
 }
