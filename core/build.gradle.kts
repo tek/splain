ea429167f65930a3d02e8a4f726e96dd68f4cb73
Peng Cheng <peng.cheng@acyclic.ai>
2023-06-10 15:39:20 -0400

    slf4j-simple scope changed to testRuntimeOnly

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,5 +8,5 @@ dependencies {
     testFixturesApi("dev.zio:zio_${vs.scalaBinaryV}:1.0.4")
 
     testFixturesApi("org.slf4j:slf4j-api:2.0.7")
-    testFixturesApi("org.slf4j:slf4j-simple:2.0.7")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.7")
 }
