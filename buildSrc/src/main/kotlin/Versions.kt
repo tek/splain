7347fbdcef3d19b308b2aa9c044fd62519cf0e6d
peng <peng.cheng@acyclic.ai>
2023-11-04 20:57:10 -0400

    v1.1.0-RC0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ class Versions(self: Project) {
     val projectRootID = "splain"
 
     val projectVMajor = "1.1.0"
-    val projectV = projectVMajor + "-SNAPSHOT"
+    val projectV = projectVMajor + "-RC0"
 
     val scalaGroup: String = self.properties.get("scalaGroup").toString()
 
